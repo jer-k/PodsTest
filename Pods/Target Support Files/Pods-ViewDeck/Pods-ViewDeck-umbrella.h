97c86161f1f7b57fa1b1dd9f224e2411267c3db1
jer-k <jeremy@carwoo.com>
2015-04-16 20:32:52 -0700

    added use_frameworks

---
+++ Pods/Target Support Files/Pods-ViewDeck/Pods-ViewDeck-umbrella.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+#import "IISideController.h"
+#import "IIViewDeckController.h"
+#import "IIWrapController.h"
+
+FOUNDATION_EXPORT double ViewDeckVersionNumber;
+FOUNDATION_EXPORT const unsigned char ViewDeckVersionString[];
+
